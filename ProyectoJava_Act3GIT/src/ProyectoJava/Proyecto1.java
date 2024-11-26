f05a2243120c9efb449c740af1445d59024523c5
Daniel Ramírez <dramcab717@g.educaand.es>
2024-11-26 13:58:31 +0100

    Modifico proyecto1

--- ProyectoJava_Act3GIT/src/ProyectoJava/Proyecto1.java
+++ ProyectoJava_Act3GIT/src/ProyectoJava/Proyecto1.java
@@ -11,8 +11,8 @@ public class Proyecto1 {
 	    public static void mostrarNumerosAleatorios() {
 	        Random random = new Random();
 	        
-	        System.out.println("20 números aleatorios entre 1 y 10:");
-	        for (int i = 0; i < 20; i++) {
+	        System.out.println("30 números aleatorios entre 1 y 10:");
+	        for (int i = 0; i < 30; i++) {
 	            int numeroAleatorio = random.nextInt(10) + 1; // Genera números entre 1 y 10
 	            System.out.print(numeroAleatorio + " ");
 	        }
